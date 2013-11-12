20f232adaaa7dd1bc83343afde8e602e3f5f77a4
dheeru0198 <dheeru0198@gmail.com>
2013-11-13 03:24:23 +0530

    added forms

---
+++ spcaqua/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from spcaqua.models import CompanyBill, PurchaseBill, CompanyBillContent, PurchaseBillContent
+
+
+class CompanyBillForm(forms.ModelForm):
+
+    class Meta:
+        model = CompanyBill
+        widgets = {
+            "bill_no": forms.HiddenInput(),
+            "total_price_words": forms.HiddenInput(),
+        }
+
+
+class CompanyBillContentForm(forms.ModelForm):
+
+    class Meta:
+        model = CompanyBillContent
+        exclude = ("company_bill",)
+
+
+class PurchaseBillForm(forms.ModelForm):
+
+    class Meta:
+        model = PurchaseBill
+        widgets = {
+            "bill_no": forms.HiddenInput(),
+            "total_price_words": forms.HiddenInput(),
+        }
+
+
+class PurchaseBillContentFrom(forms.ModelForm):
+
+    class Meta:
+        model = PurchaseBillContent
+        exclude = ("purchase_bill",)
