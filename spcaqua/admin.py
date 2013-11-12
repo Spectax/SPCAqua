d887d575846146ac8dfe11d32dc316dfc6f2679d
dheeru0198 <dheeru0198@gmail.com>
2013-11-13 01:29:38 +0530

    Registered models for admin

---
+++ spcaqua/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from models import PurchaseBill, CompanyBill, PurchaseBillContent, CompanyBillContent
+
+admin.site.register(PurchaseBill)
+admin.site.register(CompanyBill)
+admin.site.register(PurchaseBillContent)
+admin.site.register(CompanyBillContent)
