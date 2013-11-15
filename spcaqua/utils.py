dfd3044402919fabfe8ebf9b4ca0736cc775fc05
dheeru0198 <dheeru0198@gmail.com>
2013-11-16 01:45:17 +0530

    added lot models

--- spcaqua/utils.py
+++ spcaqua/utils.py
@@ -27,3 +27,14 @@ def company_bill_number():
     year = date[2]
     bill_no = str(total_bills_today + 1) + day + month + year + "C"
     return bill_no
+
+
+def lot_number():
+    total_lots_today = spcaqua.models.Lot.objects.filter(date=today).count()
+    date_string = today().strftime("%d-%m-%y")
+    date = date_string.split("-")
+    day = date[0]
+    month = date[1]
+    year = date[2]
+    lot_no = str(total_lots_today + 1) + day + month + year + "L"
+    return lot_no
