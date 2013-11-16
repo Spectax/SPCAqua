5a63648688d5b1d0c03f9eac6ac5bd0f91d68902
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-11-16 17:20:58 +0530

    bill.js corrected validation

--- spcaqua/static/js/bill.js
+++ spcaqua/static/js/bill.js
@@ -93,8 +93,8 @@ function bindKeyUp(num)
 	$('#id_form-'+i+'-quantity,#id_form-'+i+'-rate').keyup(function(){
 		var qty=Number(parseFloat($('#id_form-'+i+'-quantity').val()).toFixed(3));
 		var rate=Number(parseFloat($('#id_form-'+i+'-rate').val()).toFixed(3));
-		if(!isNaN(qty)) $('#id_form-'+i+'-quantity').val(qty);
-		if(!isNaN(rate)) $('#id_form-'+i+'-rate').val(rate);
+		//if(!isNaN(qty)) $('#id_form-'+i+'-quantity').val(qty);
+		//if(!isNaN(rate)) $('#id_form-'+i+'-rate').val(rate);
 		var price = parseFloat(qty)*parseFloat(rate);
 		if(!isNaN(price))
 			$('#id_form-'+i+'-price').val(Number((price).toFixed(3)));
@@ -105,12 +105,20 @@ function bindKeyUp(num)
 
 function validateBill()
 {
-	for(var i=0;i<5;i++)
+	var tosubmit = false;
+    for(var i=0;i<5;i++)
 	{
-		var qty = $('#id_form-'+i+'-quantity').val();
-		if(qty!=='' && qty!==null && qty!==undefined)
-			return true;
+		var qtystr = $('#id_form-'+i+'-quantity').val();
+		if(qtystr!=='' && qtystr!==null && qtystr!==undefined)
+			tosubmit = true;
+        
+        var qty=Number(parseFloat($('#id_form-'+i+'-quantity').val()).toFixed(3));
+		var rate=Number(parseFloat($('#id_form-'+i+'-rate').val()).toFixed(3));
+		if(!isNaN(qty)) $('#id_form-'+i+'-quantity').val(qty);
+		if(!isNaN(rate)) $('#id_form-'+i+'-rate').val(rate);
 	}
+    if(tosubmit)
+        return true;
 	alert('Empty bill not allowed! Enter atleast one Quantity');
 	return false;
 }
