2acea42637af1edc0e5b9eca0e3b5896daa57105
dheeru0198 <dheeru0198@gmail.com>
2013-11-18 00:51:52 +0530

    enter date manually

--- spcaqua/forms.py
+++ spcaqua/forms.py
@@ -15,13 +15,23 @@ from utils import today
 
 class CompanyBillForm(forms.ModelForm):
 
+    date = forms.DateField(initial=today, widget=forms.DateInput(format="%d/%m/%Y"), input_formats=("%d/%m/%Y",))
+
     class Meta:
         model = CompanyBill
         exclude = ("total_price", "total_quantity",)
         widgets = {
             "bill_no": forms.HiddenInput(),
-            "date": forms.HiddenInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CompanyBillForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = [
+            "bill_no",
+            "name",
+            "place",
+            "date"
+        ]
 
 
 class CompanyBillContentForm(forms.ModelForm):
@@ -33,13 +43,23 @@ class CompanyBillContentForm(forms.ModelForm):
 
 class PurchaseBillForm(forms.ModelForm):
 
+    date = forms.DateField(initial=today, widget=forms.DateInput(format="%d/%m/%Y"), input_formats=("%d/%m/%Y",))
+
     class Meta:
         model = PurchaseBill
         exclude = ("total_price", "total_quantity",)
         widgets = {
             "bill_no": forms.HiddenInput(),
-            "date": forms.HiddenInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(PurchaseBillForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = [
+            "bill_no",
+            "name",
+            "place",
+            "date"
+        ]
 
 
 class PurchaseBillContentForm(forms.ModelForm):
@@ -51,13 +71,27 @@ class PurchaseBillContentForm(forms.ModelForm):
 
 class LotForm(forms.ModelForm):
 
+    date = forms.DateField(initial=today, widget=forms.DateInput(format="%d/%m/%Y"), input_formats=("%d/%m/%Y",))
+
     class Meta:
         model = Lot
         exclude= ("total_quantity",)
         widgets = {
             "lot_no": forms.HiddenInput(),
-            "date": forms.HiddenInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(LotForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = [
+            "lot_no",
+            "company_name",
+            "company_address",
+            "vehicle_number",
+            "seal_number",
+            "driver_name",
+            "driver_phone",
+            "date"
+        ]
 
 
 class LotContentForm(forms.ModelForm):
@@ -69,12 +103,22 @@ class LotContentForm(forms.ModelForm):
 
 class IceBillForm(forms.ModelForm):
 
+    date = forms.DateField(initial=today, widget=forms.DateInput(format="%d/%m/%Y"), input_formats=("%d/%m/%Y",))
+
     class Meta:
         model = IceBill
         widgets = {
             "bill_no": forms.HiddenInput(),
-            "date": forms.HiddenInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(IceBillForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = [
+            "bill_no",
+            "company_name",
+            "company_address",
+            "date"
+        ]
 
 
 class IceBillContentForm(forms.ModelForm):
